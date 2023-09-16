01cde09c2bb4cf2e2799b2af833562f4811023b1
gibbel_hwan <126431961+Leehunhwan2001@users.noreply.github.com>
2023-09-16 15:42:56 +0900

    Add files via upload

---
+++ src/main/java/com/mycom/ICRUD.java
@@ -0,0 +1,10 @@
+package com.mycom.word;
+
+public interface ICRUD {
+    public Object add(int id);
+    public int update(int id);
+    public int delete(int id);
+    public void selectOne(int id);
+    public void saveFile();
+    public void loadFile();
+}
